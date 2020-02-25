ca7573c72c63c2cc411a0fe39417da2dd2b903eb
daniel-desouza <daniel.desouza18@gmail.com>
2020-02-25 14:20:57 -0500

    fetch user

--- src/main/java/custom/dev/service/services/UserController.java
+++ src/main/java/custom/dev/service/services/UserController.java
@@ -1,12 +1,15 @@
 package custom.dev.service.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import custom.dev.service.model.User;
@@ -19,10 +22,17 @@ public class UserController {
 	@Autowired
     UserRepository userRepository;
 
-    @RequestMapping("/api/auth/users")
+	@RequestMapping("/api/auth/users")
     public List<User> getUsers() {
     	System.out.println("in /users");
     	return userRepository.findAll();
 
+    }
+	
+	@RequestMapping("/api/auth/user/{id}")
+    public Optional<User> getUser(@PathVariable("id") String id) {
+    	System.out.println("in /user/userId");
+    	System.out.println(id);
+    	return userRepository.findById(Long.valueOf(id));
     }
 }
